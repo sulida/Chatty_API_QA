967dfaa94881cb4d6a3b7f94873e7eed27c16e93
anna <annayeshchenko7@gmail.com>
2024-07-23 17:28:53 +0200

    changes in framework

--- src/test/java/UpdateUser.java
+++ src/test/java/UpdateUser.java
@@ -2,6 +2,7 @@ import static apiUtil.ApiRequests.getRequest;
 import static apiUtil.ApiRequests.putRequest;
 import static apiUtil.UrlUtil.GET_USER_PATH;
 import static apiUtil.UrlUtil.UPDATE_OR_DELETE_USER_PATH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import user.User;
 import static user.UserDataRegistry.getUpdatedUser;
@@ -14,13 +15,18 @@ public class UpdateUser extends AdminUserTest {
     public void updateUserTest() {
         User expectedUser = getUpdatedUser();
         putRequest(UPDATE_OR_DELETE_USER_PATH + userId, expectedUser, 200, accessToken);
-        User returnedActualUser = getRequest(GET_USER_PATH, 200, accessToken).body().as(User.class);
+        User returnedUser = getRequest(GET_USER_PATH, 200, accessToken).body().as(User.class);
 
+        assertEquals(expectedUser, returnedUser);
+
+
+//        assertEquals(expectedUser.getAvatarUrl(), returnedUser.getAvatarUrl());
+//        assertEquals(expectedUser.getName(), returnedUser.getName());
+//        assertEquals(expectedUser.getSurname(), returnedUser.getSurname());
 //
-//        assertEquals(updatedUser.getAvatarUrl(), getUserResponse.jsonPath().getString("avatarUrl"));
-//        assertEquals(updatedUser.getName(), getUserResponse.jsonPath().getString("name"));
-//        assertEquals(updatedUser.getSurname(), getUserResponse.jsonPath().getString("surname"));
-//        assertThat(getUserResponse.jsonPath().getString("birthDate"), containsString(updatedUser.getBirthDate()));
+//
+//
+//        assertT hat(getUserResponse.jsonPath().getString("birthDate"), containsString(updatedUser.getBirthDate()));
 //        assertEquals(updatedUser.getPhone(), getUserResponse.jsonPath().getString("phone"));
 //        assertEquals(updatedUser.getGender(), getUserResponse.jsonPath().getString("gender"));
 //        assertEquals(updatedUser.getBackgroundUrl(), getUserResponse.jsonPath().getString("backgroundUrl"));
