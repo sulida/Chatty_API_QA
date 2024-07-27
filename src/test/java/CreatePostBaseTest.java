d76a6eeb08c5bf1ff48d6282080d6a31a117e3ad
sulida <sulida2011@gmail.com>
2024-07-28 00:22:40 +0300

    Project is updated 27.07.2024 23.22

--- src/test/java/CreatePostBaseTest.java
+++ src/test/java/CreatePostBaseTest.java
@@ -1,12 +1,8 @@
-import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import static apiUtil.ApiRequests.deleteRequest;
-import static apiUtil.ApiRequests.postRequest;
-import static apiUtil.UrlUtil.CREATE_POST_PATH;
 import static apiUtil.UrlUtil.UPDATE_OR_DELETE_OR_GET_USER_PATH;
-import static post.PostCreate.createNewPost;
 
 public class CreatePostBaseTest extends AdminUserTest {
     protected String id;
